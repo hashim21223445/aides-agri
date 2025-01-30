320f8d309b2854cb919b9f6ca2703929f465d89d
David Guillot <david.guillot@beta.gouv.fr>
2025-01-30 13:36:25 +0100

    test(ui): first basic system test on empty home page

--- apps/ui/urls.py
+++ apps/ui/urls.py
@@ -4,5 +4,5 @@ from django.views.generic import TemplateView
 
 app_label = "ui"
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="ui/base.html")),
+    path("", TemplateView.as_view(template_name="ui/base.html"), name="home"),
 ]
