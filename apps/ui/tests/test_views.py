6789bb1bef147a309992e17eedc8da73fd7ec02e
David Guillot <david.guillot@beta.gouv.fr>
2025-02-05 14:50:25 +0100

    fix(ui): typo in urls namespacing

--- apps/ui/tests/test_views.py
+++ apps/ui/tests/test_views.py
@@ -5,6 +5,6 @@ from django.urls import reverse
 
 @pytest.mark.django_db
 def test_home(client):
-    url = reverse("home")
+    url = reverse("ui:home")
     response = client.get(url)
     assert response.status_code == 200
--- apps/ui/urls.py
+++ apps/ui/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 
-app_label = "ui"
+app_name = "ui"
 urlpatterns = [
     path("", TemplateView.as_view(template_name="ui/base.html"), name="home"),
 ]
