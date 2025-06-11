14883e2ae193544b4eaa47112998f18aed8302cf
David Guillot <david.guillot@beta.gouv.fr>
2025-06-11 14:57:34 +0200

    Ajouter un QR code visible à l'impression de toutes les pages pour revenir sur le site (#99)
    * feat(deps): setup qrcode
    
    * refactor(ui): delete unused CSS and icons
    
    * feat(ui): DSFR-style CSS class for print-only content
    
    * feat(ui): on all pages, print-only QR code to get back on the site
    
    * feat(agri): RWD adjustment on results page

--- apps/ui/context_processors.py
+++ apps/ui/context_processors.py
@@ -1,8 +1,12 @@
+import qrcode
 from django.conf import settings
 
 
 def ui_tools_tokens(request):
+    qr = qrcode.QRCode(image_factory=qrcode.image.svg.SvgPathImage, box_size=7)
+    qr.add_data(request.build_absolute_uri())
     return {
+        "qrcode": qr.make_image().to_string(encoding="unicode"),
         "UI_ENVIRONMENT": settings.ENVIRONMENT,
         "UI_MATOMO_SITE_ID": settings.MATOMO_SITE_ID,
         "UI_SENTRY_PUBLIC_KEY": settings.SENTRY_UI_PUBLIC_KEY,
